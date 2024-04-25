352ff1d10c08f85562cbd936c1d3eab7107474ae
Ibraheem Cazalas <71295552+ICAZ117@users.noreply.github.com>
2024-04-25 14:54:54 -0400

    Update images_view_page.dart

--- lib/src/images_view_page.dart
+++ lib/src/images_view_page.dart
@@ -224,7 +224,16 @@ class _ImagesViewPageState extends State<ImagesViewPage>
               style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           )
-        : buildGridView();
+        : Scaffold(
+            backgroundColor: widget.appTheme.primaryColor,
+            appBar: SequentialAppBar(
+              leftFunction: widget.leftFunction,
+              leftIcon: Icons.clear_rounded,
+              rightFunction: widget.rightFunction,
+              rightText: 'Next',
+            )
+            body: buildGridView(),
+        );
   }
 
   ValueListenableBuilder<bool> buildGridView() {
@@ -765,4 +774,66 @@ class _ImagesViewPageState extends State<ImagesViewPage>
 
   @override
   bool get wantKeepAlive => true;
+}
+
+import 'package:blend/global_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+
+class SequentialAppBar extends StatelessWidget {
+  final Function leftFunction;
+  final IconData leftIcon;
+  final Function rightFunction;
+  final String rightText;
+
+  const SequentialAppBar({
+    super.key,
+    required this.leftFunction,
+    required this.leftIcon,
+    required this.rightFunction,
+    required this.rightText,
+  });
+
+  @override
+  PreferredSize build(BuildContext context) {
+    final provider = Provider.of<GlobalProvider>(context);
+    final _blendUser = provider.blendUser;
+
+    return PreferredSize(
+      preferredSize: Size.fromHeight(kToolbarHeight),
+      child: AppBar(
+        leading: IconButton(
+          icon: Icon(leftIcon),
+          onPressed: () {
+            leftFunction();
+          },
+        ),
+        centerTitle: true,
+        title: Text(
+          'New Post',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        actions: <Widget>[
+          // button using user's profile picture
+          GestureDetector(
+            onTap: () {
+              rightFunction();
+            },
+            child: Container(
+                margin: EdgeInsets.only(right: 20),
+                child: Text(
+                  rightText,
+                  style: TextStyle(
+                    fontSize: 17,
+                  ),
+                )),
+          ),
+        ],
+      ),
+    );
+  }
 }
